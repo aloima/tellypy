4b699337cbc45b9855034df057a512f1a48fa0d1
aloima <acar.aloima@gmail.com>
2026-01-19 22:40:17 +0300

    feat: complete client.connect() method
    * Add set_info and protocol options
    * Add error handling

--- src/tellypy/__init__.py
+++ src/tellypy/__init__.py
@@ -1,5 +1,2 @@
 from .kinds import Value, Kind
 from .client import Client, Protocol
-
-__version__ = "0.1.0"
-__author__ = "aloima"
--- src/tellypy/client.py
+++ src/tellypy/client.py
@@ -1,6 +1,8 @@
-from .kinds import Value, Kind
+from importlib.metadata import version
 from enum import Enum
 import socket
+
+from .kinds import Value, Kind
 
 
 class Protocol(Enum):
@@ -9,21 +11,49 @@ class Protocol(Enum):
 
 
 class Client:
-    connected: bool
-    socket: socket.client
-    protocol: Protocol
+    __preconnected: bool = False
+    __connected: bool = False
+    __socket: socket.client = None
+    __protocol: Protocol = None
 
-    def __init__(self):
-        self.connected = False
+    __host: str
+    __port: int
 
-    def connect(self, host: str, port: int, proto: Protocol = Protocol.RESP2):
-        self.protocol = proto
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((host, port))
+    def __init__(self, host: str, port: int = 6379):
+        self.__host = host
+        self.__port = port
+
+    def connect(self, set_info: bool = True,
+                protocol: Protocol = Protocol.RESP2) -> bool:
+        try:
+            self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__socket.connect((self.__host, self.__port))
+            self.__preconnected = True
+            self.execute_command(f"HELLO {protocol.value}")
+
+            if set_info:
+                lib_name = __package__.split(".")[0]
+                lib_ver = version(lib_name)
+                self.execute_command(f"CLIENT SETINFO LIB-NAME {lib_name}")
+                self.execute_command(f"CLIENT SETINFO LIB-VERSION {lib_ver}")
+
+        except Exception as error:
+            self.__connected = False
+            raise error
+
+        else:
+            self.__connected = True
+            return True
+
+    def is_connected(self) -> bool:
+        return self.__connected
 
     def execute_command(self, command: str):
+        if not self.__preconnected:
+            return
+
         data = [Value(data, Kind.BULK_STRING) for data in command.split()]
-        self.client.send(Value(data, Kind.ARRAY).to_raw())
-        res = self.client.recv(1024)
+        self.__socket.send(Value(data, Kind.ARRAY).to_raw())
+        res = self.__socket.recv(1024)
 
         return res
