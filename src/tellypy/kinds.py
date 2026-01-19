314e174637fb8011fd5bb2af8260203a8580840b
aloima <acar.aloima@gmail.com>
2026-01-20 00:04:37 +0300

    feat: add Value.from_raw method

--- src/tellypy/kinds.py
+++ src/tellypy/kinds.py
@@ -1,15 +1,18 @@
 from typing import Any
 from enum import Enum
 
+r_a = ord("\r")
+n_a = ord("\n")
+
 
 class Kind(Enum):
-    NULL = 0
-    INTEGER = 1
-    DOUBLE = 2
-    SIMPLE_STRING = 3
-    BULK_STRING = 4
-    SIMPLE_ERROR = 5
-    ARRAY = 6
+    NULL = ord("_")
+    INTEGER = ord(":")
+    DOUBLE = ord(",")
+    SIMPLE_STRING = ord("+")
+    BULK_STRING = ord("$")
+    SIMPLE_ERROR = ord("-")
+    ARRAY = ord("*")
     # BOOLEAN = 7
     # HASHTABLE = 8
     # LIST = 9
@@ -22,6 +25,74 @@ class Value:
     def __init__(self, data: Any, kind: Kind):
         self.kind = kind
         self.data = data
+
+    @staticmethod
+    def from_raw(value: memoryview) -> tuple["Value", int]:
+        match value[0]:
+            case Kind.NULL.value:
+                return Value(None, Kind.NULL)
+
+            case Kind.INTEGER.value:
+                idx = value.index(r_a)
+
+                if idx != -1 and value[idx + 1] == n_a:
+                    data = value[1:idx]
+                    n = idx + 2
+
+                    return Value(int(data), Kind.INTEGER), n
+
+            case Kind.DOUBLE.value:
+                idx = value.index(r_a)
+
+                if idx != -1 and value[idx + 1] == n_a:
+                    data = value[1:idx]
+                    n = idx + 2
+
+                    return Value(float(data), Kind.DOUBLE), n
+
+            case Kind.SIMPLE_STRING.value:
+                idx = value.index(r_a)
+
+                if idx != -1 and value[idx + 1] == n_a:
+                    data = value[1:idx].tobytes()
+                    n = idx + 2
+
+                    return Value(data.decode("utf-8"), Kind.SIMPLE_STRING), n
+
+            case Kind.BULK_STRING.value:
+                start = value.index(r_a)
+                length = int(value[1:start])
+
+                data = value[(start + 2):(start + length)].tobytes()
+                n = start + length + 4
+
+                return Value(data.decode("utf-8"), Kind.BULK_STRING), n
+
+            case Kind.SIMPLE_ERROR.value:
+                idx = value.index(r_a)
+
+                if idx != -1:
+                    data = value[1:idx].tobytes()
+                    n = idx + 2
+
+                    return Value(data.decode("utf-8"), Kind.SIMPLE_ERROR), n
+
+            case Kind.ARRAY.value:
+                idx = value.index(r_a)
+
+                if idx != -1 and value[idx + 1] == n_a:
+                    count = int(value[1:idx])
+                    data: list[Value] = []
+                    total = idx + 2
+
+                    for _ in range(count):
+                        subvalue, parsed = Value.from_raw(value[total:])
+                        data.append(subvalue)
+                        total += parsed
+
+                    return Value(data, Kind.SIMPLE_ERROR), total
+
+        return Value(None, Kind.NULL), 0
 
     def to_raw(self) -> str:
         match self.kind:
